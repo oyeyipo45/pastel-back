5a3eb344e729c04ee4c301bc5f30290829796ec9
Damilola Oyeyipo <damilola@7am.io>
2024-08-29 06:26:27 +0100

    add data source configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,13 @@ import { Environment } from '../common/types/env.enums';
 export const configuration = (): {
   nodeENV: Environment;
   appPort: number;
+  username: string;
+  db_name: string;
+  password: string;
 } => ({
   nodeENV: <Environment>process.env.NODE_ENV,
   appPort: Number(process.env.APP_PORT),
+  username: String(process.env.USERNAME),
+  db_name: String(process.env.DB_NAME),
+  password: String(process.env.PASSWORD),
 });
